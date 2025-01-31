03f5751d87932bf89608ff675d349c528eb7ab0a
Facundo Sura <facundomesura@gmail.com>
2025-01-31 00:52:02 -0300

    homepage product fetch

--- e-commerce/src/app/page.tsx
+++ e-commerce/src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
     <div>
-      
+      <div>carrusel o banners promocionales</div>
+      <div>productos destacados o mas vendidos</div>
+      <div>llamados a la acción (CTA) como "Comprar ahora" o "Explorar colección"</div>
+      <div>breve introducción a la marca o negocio</div>
     </div>
   );
 }
