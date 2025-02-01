632fe4bb6d0b07edbee783730755fe712a42781b
Facundo Sura <facundomesura@gmail.com>
2025-02-01 00:26:41 -0300

    home page, carusel y heroSection

--- e-commerce/src/app/page.tsx
+++ e-commerce/src/app/page.tsx
@@ -1,10 +1,25 @@
+import Carrusel from "@/components/home/carousel";
+
 export default function Home() {
   return (
     <div>
-      <div>carrusel o banners promocionales</div>
-      <div>productos destacados o mas vendidos</div>
-      <div>llamados a la acción (CTA) como "Comprar ahora" o "Explorar colección"</div>
-      <div>breve introducción a la marca o negocio</div>
+      <div>
+        <h1 className="text-center text-xl font-bold">
+          carrusel o banners promocionales
+        </h1>
+        <Carrusel />
+      </div>
+      <div>
+        <h1 className="text-center">productos destacados o mas vendidos</h1>
+      </div>
+      <div>
+        <h1 className="text-center">
+          llamados a la acción (CTA) como "Comprar ahora" o "Explorar colección"
+        </h1>
+      </div>
+      <div>
+        <h1 className="text-center">breve introducción a la marca o negocio</h1>
+      </div>
     </div>
   );
 }
